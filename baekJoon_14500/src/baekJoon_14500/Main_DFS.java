1121a7131bb9057d3446276270e89672203649b0
hys0254 <hys0254@naver.com>
2020-09-11 03:29:42 +0900

    백준 -14500 (solving...)

--- baekJoon_14500/src/baekJoon_14500/Main_DFS.java
+++ baekJoon_14500/src/baekJoon_14500/Main_DFS.java
@@ -12,6 +12,7 @@ public class Main_DFS {
 	static int max = Integer.MIN_VALUE;
 	static int maxRow;
 	static int maxColumn;
+	static int exceptTemp;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -33,35 +34,77 @@ public class Main_DFS {
 				numPaper[i][j] = 0;
 			}
 		}
-		search(0, 0, 0, 0);
+		for (int i = 0; i < maxColumn; i++) {
+			
+			for (int j = 0; j < maxRow; j++) {
+			
+				search(i, j, 0, 1);
+				exceptSearch(i, j);
+			}
+		}
 
 	}
 
 	public static void search(int startRow, int startColumn, int temp, int cnt) {
-		visitCheck[startRow][startColumn] = 1;
 		int nextRow;
 		int nextColumn;
 		
 		for (int i = 0; i < 4; i++) {
 			nextRow = startRow + dir[i][0];
 			nextColumn = startColumn + dir[i][1];
-			if (nextRow > maxRow || nextColumn > maxColumn || nextRow < 0 || nextColumn < 0||(visitCheck[nextRow][nextColumn]==1)) {
-				visitCheck[startRow][startColumn] = 0;
+			if (nextRow > maxRow || nextColumn > maxColumn || nextRow < 0 || nextColumn < 0) {
+				continue;
+			}
+			if((visitCheck[nextRow][nextColumn]==1)) {
 				continue;
 			}
 
 			temp += numPaper[startRow][startColumn];
 			if (cnt == 4) {
-				visitCheck[startRow][startColumn] = 0;
 				if (temp > max) {
 					max = temp;
 				}
 				return;
 			} else {
+				visitCheck[startRow][startColumn] = 1;
 				search(nextRow, nextColumn, temp, cnt + 1);
+				visitCheck[startRow][startColumn]=0;
 			}
 		}
-		if(startRow+1<maxRow) {	search(startRow+1,startColumn,temp,cnt);}
-		else if(startRow+1>maxRow) {search(0,startColumn+1,temp,cnt);}
+		
+	}
+	public static void exceptSearch(int x, int y ) {
+		 int temp = 0;
+		    // 1. ぬ
+		    if(x>=0 && x<maxRow && y>=0 && y+2<maxColumn){
+		        temp = numPaper[x][y] + numPaper[x][y+1] + numPaper[x][y+2] + numPaper[x+1][y+1];
+		        if (max < temp) {
+					max = temp;
+				}
+		    }
+
+		    // 2. た
+		    if(x >= 0 && x+2 <maxRow && y>=0 && y+1<maxColumn){
+		        temp = numPaper[x][y] + numPaper[x+1][y] + numPaper[x+2][y] + numPaper[x+1][y+1];
+		        if (max < temp) {
+					max = temp;
+				}
+		    }
+
+		    // 3. で
+		    if(x-1 >= 0&& x <maxRow && y >=0 && y+2 <maxColumn){
+		        temp = numPaper[x][y] + numPaper[x][y+1] + numPaper[x][y+2] + numPaper[x-1][y+1];
+		        if (max < temp) {
+					max = temp;
+				}
+		    }
+
+		    // 4. っ
+		    if(x-1 >= 0 && x+1 <=maxRow && y >=0 && y+1 <=maxColumn){
+		        temp = numPaper[x][y] + numPaper[x][y+1] + numPaper[x-1][y+1] + numPaper[x+1][y+1];
+		        if (max < temp) {
+					max = temp;
+				}
+		    }
 	}
 }
