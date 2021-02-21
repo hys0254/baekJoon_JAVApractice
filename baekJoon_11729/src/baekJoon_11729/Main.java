c34d58ef8001203e7783089152487e9a76709c2b
hys0254 <hys0254@naver.com>
2021-02-22 00:35:42 +0900

    백준 - 11729 (re)

---
+++ baekJoon_11729/src/baekJoon_11729/Main.java
@@ -0,0 +1,30 @@
+package baekJoon_11729;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		sb.append((int)(Math.pow(2, N)-1)).append('\n');			
+		hanoi(N,1,3,2);
+		System.out.println(sb);
+	}
+	public static void hanoi (int size, int from, int to, int mid) {
+		if(size==1) {
+			sb.append(from).append(" ").append(to).append("\n");
+			return;
+		}
+				
+		
+		hanoi(size-1,from,mid,to);
+		sb.append(from).append(" ").append(to).append("\n");
+		hanoi(size-1,mid,to,from);
+		
+		
+		
+	}
+}
