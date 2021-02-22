4bfec4dae31649fe4694f52aff0db0fe94b559c6
hys0254 <hys0254@naver.com>
2021-02-23 00:07:39 +0900

    백준 - 2798

--- baekJoon_2798/src/baekJoon_2798/Main.java
+++ baekJoon_2798/src/baekJoon_2798/Main.java
@@ -7,20 +7,33 @@ import java.util.StringTokenizer;
 public class Main {
 
 	public static void main(String[] args)throws Exception {
-		// 첫째줄 입력 : 카드의 개수 N 3~100, 최대값 10~300000
-				// 둘째줄 입력 : 카드에 쓰여있는 수 100000을 넘지 않는 양의 정수
+		
 				BufferedReader rb = new BufferedReader(new InputStreamReader(System.in));
 				
 				StringTokenizer st = new StringTokenizer(rb.readLine());
 				int numCard = Integer.parseInt(st.nextToken());
 				int limit = Integer.parseInt(st.nextToken());
+				int maxSum = Integer.MIN_VALUE;
 				int[] cardSet = new int[numCard];
 				st = new StringTokenizer(rb.readLine());
 				for(int i =0 ;i<numCard;i++) {
 					cardSet[i]=Integer.parseInt(st.nextToken());
 				}
 				
-				
+				for(int i = 0; i<numCard;i++) {
+					for(int j = 0; j<numCard;j++) {
+						if(i==j) {continue;}
+						for(int k = 0; k<numCard;k++) {
+							if(j==k||k==i) {continue;}
+							int compare = cardSet[i] + cardSet[j] + cardSet[k];
+							if(compare>maxSum&&compare<=limit) {
+							
+								maxSum=compare;
+							}
+						}
+					}
+				}
+				System.out.println(maxSum);
 				
 	}
 
